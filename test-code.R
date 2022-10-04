718f8578466068fe8001b4e0a04336dafe5b088d
liz-fw <lstebbins@flowwest.com>
2022-10-04 13:24:19 -0700

    added square function and plot

--- test-code.R
+++ test-code.R
@@ -2,3 +2,14 @@
 # for practicing branches in git 
 
 plot(seq(1:30), (seq(1:30)^2), type='l')
+
+square <- function(x) {
+  # inputs: x is a vector of values to be squared
+  y <- x^2
+  return(y)
+}
+
+# example:
+x <- seq(1:10)
+y <- square(x)
+plot(x, y, type = 'l')
