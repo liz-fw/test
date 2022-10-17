2644a413381e6aed0d3f431dece87c17f6ac8bb8
liz-fw <lstebbins@flowwest.com>
2022-10-17 12:30:58 -0700

    deleted extraneous code so only power function remains

--- R/test-code.R
+++ R/test-code.R
@@ -1,28 +1,10 @@
 # this is a test file
 # for practicing branches in git 
+# and practicing unit-testing
 
 # load libraries
 library(ggplot2)
 library(roxygen2)
-
-plot(seq(1:30), (seq(1:30)^2), type='l')
-
-square <- function(x) {
-  # inputs: x is a vector of values to be squared
-  y <- x^2
-  return(y)
-}
-
-# example:
-x <- seq(1:10)
-y <- square(x)
-plot(x, y, type = 'l')
-
-
-# for erin's review -------------------------------------------------------
-
-# practicing a pull request
-# adding a new function here to generalize the power function
 
 #' Raise a vector of values to a power
 #' 
